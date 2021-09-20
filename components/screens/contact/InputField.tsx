8e45915848b27d9f852d839707aa571673b25a00
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-09-20 21:19:56 +0530

    improve input component

--- components/screens/contact/InputField.tsx
+++ components/screens/contact/InputField.tsx
@@ -14,7 +14,7 @@ export const InputField: React.FC<Props> = ({ id, value, onChange, label, contai
     <div className={`bg-gray-444 p-20 flex flex-col rounded-default w-5/12 w-full ${containerClasses || ''}`}>
       <label className='mb-5 text-gray-f2f text-body-sm' htmlFor={id}>{label}</label>
       <input className='bg-gray-444 border-b border-gray-e0e text-white font-light text-body-lg' id={id} value={value} onChange={onChange} />
-      <p className='text-red-300 text-body-sm font-medium'>{error}</p>
+      <p className='text-gray-999 text-body-sm font-medium mt-5'>{error}</p>
     </div>
   );
 };

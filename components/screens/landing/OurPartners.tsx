7f8275a731105c9ad5d755bc561d6db89b4672c7
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-10-04 22:35:39 +0530

    create dynamic pages via markdown

--- components/screens/landing/OurPartners.tsx
+++ components/screens/landing/OurPartners.tsx
@@ -9,17 +9,17 @@ export const OurPartners: React.FC<Props> = () => {
       <h3 className="text-h3 sm:text-h4 font-semibold text-gray-e0e text-center">
         Our Partners
       </h3>
-      <div className="flex flex-wrap items-center justify-around mt-30">
+      <div className="flex flex-wrap justify-around mt-30">
         {partners.map((partner, i) => (
           <div key={i} className="mt-20">
-            <div className="w-48 h-48 overflow-hidden rounded-default">
+            <div className="w-48 overflow-hidden rounded-default">
               <img
                 src={partner.img_src}
                 className="w-full h-fill object-cover"
                 alt={`${partner.name} photo`}
               />
             </div>
-            <h5 className="text-center text-h5 text-gray-f2f font-semibold mt-25">
+            <h5 className="text-center text-h5 text-gray-f2f font-semibold mt-25 w-48">
               {partner.name}{" "}
             </h5>
           </div>
---
+++ shared/mdxUtils.ts
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+
+// MARKDOWN_PATH is useful when you want to get the path to a specific file
+export const MARKDOWN_PATH = path.join(process.cwd(), 'markdowns');
+
+// markdownFilePaths is the list of all mdx files inside the MARKDOWN_PATH directory
+export const markdownFilePaths = fs
+  .readdirSync(MARKDOWN_PATH)
+  // Only include md(x) files
+  .filter((path) => /\.mdx?$/.test(path));

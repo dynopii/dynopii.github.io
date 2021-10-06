1499d5205a537b1b2c96748701730ae344f2e501
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-10-06 17:08:36 +0530

    added api key in urls for ghost api

--- shared/urls.ts
+++ shared/urls.ts
@@ -1,4 +1,5 @@
 export const urls = {
-  GHOST_BLOGS_API: `https://blog.dynopii.com/ghost/api/v3/content/posts/?key=${process.env.NEXT_PUBLIC_GHOST_API_KEY}`,
+  // GHOST_BLOGS_API: `https://blog.dynopii.com/ghost/api/v3/content/posts/?key=${process.env.NEXT_PUBLIC_GHOST_API_KEY}`,
+  GHOST_BLOGS_API: `https://blog.dynopii.com/ghost/api/v3/content/posts/?key=51f7ea9bdb82bc27bcea24ab31`,
   GHOST_BLOGS: 'https://blog.dynopii.com/'
 };

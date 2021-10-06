94cce47eed4073bec7292aa81775fa2eb756dd32
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-10-06 09:27:27 +0530

    rename markdowns folder to md_pages

--- shared/mdxUtils.ts
+++ shared/mdxUtils.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 // MARKDOWN_PATH is useful when you want to get the path to a specific file
-export const MARKDOWN_PATH = path.join(process.cwd(), 'markdowns');
+export const MARKDOWN_PATH = path.join(process.cwd(), 'md_pages');
 
 // markdownFilePaths is the list of all mdx files inside the MARKDOWN_PATH directory
 export const markdownFilePaths = fs

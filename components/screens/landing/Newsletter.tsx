48bed52d8303ef46b0f649f3cf9c1395bead6016
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-10-07 11:31:54 +0530

    removed unused code from newsletter

--- components/screens/landing/Newsletter.tsx
+++ components/screens/landing/Newsletter.tsx
@@ -1,17 +1,9 @@
-import React, { useState } from "react";
-import * as Yup from "yup";
+import React from "react";
 import { landingContents } from '../../../shared/contents';
 
 interface Props { }
 
-// Validation Schema for Newsletter form
-const ValidationSchema = Yup.object().shape({
-  email: Yup.string().required("Please enter email."),
-});
-
 export const Newsletter: React.FC<Props> = () => {
-  const [input, setInput] = useState('');
-
 
   const handleSubmit = (e: any) => {
     window.open(
@@ -31,77 +23,6 @@ export const Newsletter: React.FC<Props> = () => {
         {landingContents.newsletter.description}
       </p>
 
-      {/* <Formik
-        initialValues={{
-          email: "",
-        }}
-        validateOnChange={false}
-        validateOnBlur={false}
-        validateOnMount={false}
-        validationSchema={ValidationSchema}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setSubmitting(true);
-          // setTimeout(async () => {
-          //   try {
-          //     const { data }: any = await axios({
-          //       url: 'https://formspree.io/f/xdopdwyy',
-          //       method: 'POST',
-          //       data: values
-          //     });
-          //     if (data.ok) {
-          //       resetForm({});
-          //       toast('Successfully subscribed.', { type: 'success' });
-          //     }
-          //   } catch (e) {
-          //     // console.log(e);
-          //     toast('Error, Please try again later.', { type: 'error' });
-          //   }
-          //   setSubmitting(false);
-          // }, 100);
-          window.open(
-            `https://www.tinyletter.com/dynopii`,
-            'popupwindow',
-            'scrollbars=yes,width=800,height=600'
-          );
-          setSubmitting(false);
-          // return true;
-        }}
-      >
-        {({
-          values,
-          errors,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          setFieldValue,
-          isSubmitting,
-        }) => (
-          <form onSubmit={handleSubmit}>
-           
-
-            <div className="mt-40 sm:flex-col sm:items-stretch flex items-center bg-white p-2 rounded-default w-full">
-              <input
-                id="email"
-                placeholder="Enter your email address"
-                value={values.email}
-                type='email'
-                required={true}
-                onChange={handleChange}
-                className="py-10 w-80 sm:w-60 xs:w-56 mr-2 sm:mr-0 px-10 focus:outline-none focus:ring-2 focus:ring-brand focus:border-transparent rounded-default"
-              />
-              <button
-                disabled={isSubmitting}
-                type='submit'
-                className="sm:mt-10 bg-brand px-50 py-10 rounded-default text-white btn--gradient"
-              >
-                Subscribe
-              </button>
-            </div>
-
-          </form>
-        )}
-        </Formik> */}
-
       <form
         action={`https://www.tinyletter.com/dynopii`}
         method="post"
@@ -112,10 +33,8 @@ export const Newsletter: React.FC<Props> = () => {
 
         <div className="mt-40 sm:flex-col sm:items-stretch flex items-center bg-white p-2 rounded-default w-full">
           <input
-            // id="email"
             placeholder="Enter your email address"
             aria-label="Email address"
-            // placeholder="Enter your email..."
             name="email"
             type="text"
             required
@@ -123,7 +42,6 @@ export const Newsletter: React.FC<Props> = () => {
             className="py-10 w-80 sm:w-60 xs:w-56 mr-2 sm:mr-0 px-10 focus:outline-none focus:ring-2 focus:ring-brand focus:border-transparent rounded-default"
           />
           <button
-            // disabled={isSubmitting}
             type='submit'
             className="sm:mt-10 bg-brand px-50 py-10 rounded-default text-white btn--gradient"
           >

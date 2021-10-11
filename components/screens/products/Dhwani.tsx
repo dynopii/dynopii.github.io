6f599ad080c47d5418cd8477ba270791c4acd91c
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-10-11 23:01:13 +0530

    dhwani header webgl

--- components/screens/products/Dhwani.tsx
+++ components/screens/products/Dhwani.tsx
@@ -4,15 +4,34 @@ import { FeatureCard } from "./FeatureCard";
 import Image from "next/image";
 import DhwaniSVG from "../../../public/svgs/dhwanilogo.svg";
 import { dhwaniCard, productsContent } from "../../../shared/contents";
+import Exp from "../../animated-gradient";
 interface Props {}
 
 export const Dhwani: React.FC<Props> = () => {
   return (
     <div className="bg-gray-333 rounded-default overflow-hidden">
-      <div className="dhwani-cover-bg h-96 flex items-center justify-center overflow-hidden">
-        {/* <div className=" h-96 flex items-center justify-center overflow-hidden relative"> */}
-        {/* <Image src={DhwaniSVG} alt="logo" width={115} height={115} />
-        <h4 className="text-h4 text-gray-f2f font-normal ml-15">Dhwani</h4> */}
+      <div className="dhwani-cover-bg h-96 flex items-center justify-center overflow-hidden relative">
+        {/* <div
+        className=" h-96 flex items-center justify-center overflow-hidden relative"
+        //         style={{
+        //           backgroundImage: `#B5179E`,
+        //           backgroundImage: `radial-gradient(at 29% 36%, hsla(313,91%,61%,1) 0, transparent 47%),
+        // radial-gradient(at 74% 72%, hsla(232,94%,65%,1) 0, transparent 100%),
+        // radial-gradient(at 29% 10%, hsla(219,92%,72%,1) 0, transparent 53%),
+        // radial-gradient(at 31% 77%, hsla(147,71%,63%,1) 0, transparent 42%)`,
+        //         }}
+      > */}
+        <Image
+          className="z-1"
+          src={DhwaniSVG}
+          alt="logo"
+          width={115}
+          height={115}
+        />
+        <h4 className="z-1 text-h4 text-gray-f2f font-normal ml-15">Dhwani</h4>
+        <div className="absolute inset-0">
+          <Exp />
+        </div>
       </div>
 
       <div className="p-30">

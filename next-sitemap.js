e8567d00094e18b7e0400d68f30fe333a51bda51
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-10-22 14:24:23 +0530

    sitemap fix

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,4 +3,24 @@ const url = 'https://dynopii-web.vercel.app/'
 module.exports = {
     siteUrl: url,
     generateRobotsTxt: true,
+    changefreq: 'daily',
+    transform: async (config, path) => {
+        console.log(path)
+        if(path === '/'){
+            return {
+                loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+                changefreq: config.changefreq,
+                priority: 1,
+                lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+                alternateRefs: config.alternateRefs ?? [],
+            }
+        }
+        return {
+          loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+          changefreq: config.changefreq,
+          priority: config.priority,
+          lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+          alternateRefs: config.alternateRefs ?? [],
+        }
+      },
 }

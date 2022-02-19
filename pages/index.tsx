8e7cd1f9ee4b13abe9c540b93d902445d74a1d04
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2022-02-19 13:21:51 +0530

    remove unused code/imports.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Header } from "../components/Header";
-import Main2 from "../components/Main";
+import Main from "../components/Main";
 import { Footer } from "../components/layout/Footer";
 import { Testimonials } from "../components/screens/landing/Testimonials";
 import { OurPartners } from "../components/screens/landing/OurPartners";
 import { OurProducts } from "../components/screens/landing/OurProducts";
 import { OurSolutionsBacked } from "../components/screens/landing/OurSolutionsBacked";
-import { OurCustomers } from "../components/screens/landing/OurCustomers";
 import { Newsletter } from "../components/screens/landing/Newsletter";
 
 const Home: NextPage = () => {
@@ -17,13 +16,11 @@ const Home: NextPage = () => {
         <title>Dynopii | Redefining the way you communicate.</title>
       </Head>
       <Header />
-      {/* <Main /> */}
-      <Main2 />
+      <Main />
       <section className="mx-auto p-30" style={{ maxWidth: "1200px" }}>
         <OurPartners />
         <OurProducts />
         <OurSolutionsBacked />
-        {/* <OurCustomers /> */}
       </section>
       <Testimonials />
       <section className="mx-auto p-30" style={{ maxWidth: "1200px" }}>

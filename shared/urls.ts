2f9ff0001281850964b79c10114043084d2482a2
xprilion <xprilion@gmail.com>
2023-09-08 00:56:50 +0530

    blogs fixed

--- shared/urls.ts
+++ shared/urls.ts
@@ -1,6 +1,6 @@
 export const urls = {
   // GHOST_BLOGS_API: `https://blog.dynopii.com/ghost/api/v3/content/posts/?key=${process.env.NEXT_PUBLIC_GHOST_API_KEY}`,
-  GHOST_BLOGS_API: `https://blog.dynopii.com/ghost/api/v3/content/posts/?key=51f7ea9bdb82bc27bcea24ab31`,
+  GHOST_BLOGS_API: `https://blog.dynopii.com/ghost/api/v3/content/posts/?key=213e30e9d6e62789e4f275636f`,
   GHOST_BLOGS: 'https://blog.dynopii.com/',
   DYNOPII_TWITTER: 'https://twitter.com/dynopii',
   DYNOPII_LINKEDIN: 'https://in.linkedin.com/company/dynopii',
